25f667eb180a3a89cab55d56e1c7993f4933f041
Isaac <your.email@example.com>
2024-11-26 18:01:01 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+
+const headquarters = 42;
+
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - headquarters);
+}
+// test/index.test.js
+
+const { expect } = require('chai');
+const fs = require('fs');
+const path = require('path');
+
+// Require the index.js file to be tested
+const { distanceFromHqInBlocks } = require('../index');
+
+// Read the content of index.js
+const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8');
+
+describe('index.js', function () {
+  describe('distanceFromHqInBlocks()', function () {
+    it('returns a distance in blocks', function () {
+      expect(distanceFromHqInBlocks(43)).to.equal(1);
+      expect(distanceFromHqInBlocks(50)).to.equal(8);
+      expect(distanceFromHqInBlocks(34)).to.equal(8);
+    });
+  });
+});
+
+
